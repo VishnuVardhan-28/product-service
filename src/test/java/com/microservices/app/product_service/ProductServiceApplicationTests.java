41ce5f7c37ce3d0f4503745a4ae5aa3de2a6d223
VishnuVardhan-28 <dhanduvishnu@gmail.com>
2025-11-15 20:07:04 +0530

    Implemented product-service microservice with mongoDB as database and created 2 apis for creating a product and fetching the list of products.
    Stopped at writing automated tests using TestContainer as i need to learn Junit first to understand the basics.

--- src/test/java/com/microservices/app/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/microservices/app/product_service/ProductServiceApplicationTests.java
@@ -2,9 +2,16 @@ package com.microservices.app.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0.9");
 
 	@Test
 	void contextLoads() {
